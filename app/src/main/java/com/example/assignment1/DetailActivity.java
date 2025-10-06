f46da687b1ca533fbd581ca5a89e4f10eeffd8a7
Woohyuk Song <harrywsong@naver.com>
2025-10-06 11:19:44 -0400

    added logic to DetailActivity.java

--- app/src/main/java/com/example/assignment1/DetailActivity.java
+++ app/src/main/java/com/example/assignment1/DetailActivity.java
@@ -1,4 +1,28 @@
 package com.example.assignment1;
 
-public class DetailActivity {
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private ListView paymentList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        paymentList = findViewById(R.id.paymentList);
+
+        ArrayAdapter<Payment> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                MainActivity.paymentList
+        );
+
+        paymentList.setAdapter(adapter);
+    }
 }
